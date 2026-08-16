4bf04afee6dcf4d3852afbfd2dd493b3fec2f2e2
Samson Ajifowoke <samsonajifowoke@gmail.com>
2026-08-16 02:34:33 +0100

    chore: disable pool and add connect args to task db engine

--- app/task/db.py
+++ app/task/db.py
@@ -1,5 +1,6 @@
 from redis import Redis
 from redis.retry import Retry
+from sqlalchemy.pool import NullPool
 from redis.connection import ConnectionPool
 from sqlalchemy import Engine, create_engine
 from redis.backoff import ExponentialBackoff
@@ -12,11 +13,8 @@ from app.core.config import get_settings
 
 db_engine: Engine = create_engine(
     url=get_settings().SYNC_DB_URL,
-    pool_size=10,
-    pool_timeout=10.0,
-    pool_pre_ping=True,
-    max_overflow=5,
-    connect_args={"options": "-c timezone=utc"},
+    poolclass=NullPool,
+    connect_args=get_settings().DB_CONN_ARGS,
 )
 
 
